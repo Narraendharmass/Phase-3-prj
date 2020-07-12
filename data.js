ea728d11ea90f643ac9ef22ea1d02e770e05527f
Narraendharmass <67266747+Narraendharmass@users.noreply.github.com>
2020-07-12 13:12:41 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,82 @@
+var jsonData =[
+    {
+        "q" : "1.JSON stands for:",
+        "opt1" :"Java Standard Output Network",
+        "opt2" :"JavaScript Object Notation",
+        "opt3" :"Java Source Open Network",
+        "answer" :"JavaScript Object Notation"
+
+    },
+    {
+        "q" : "2. The MIME type of JSON is -",
+        "opt1" :"application/json",
+        "opt2" :"application/x-json",
+        "opt3" :" application/javascript",
+        "answer" :"application/json"
+
+    },
+    {
+        "q" : "3. Which of the following is not a type in JSON?",
+        "opt1" :"date",
+        "opt2" :"Object",
+        "opt3" :" string",
+        "answer" :"date"
+
+    },
+    {
+        "q" : "4. Who is the Father of JSON ?",
+        "opt1" :"Douglas Crockford",
+        "opt2" :"Rasmus Lerdorf",
+        "opt3" :" Dennis Ritchie",
+        "answer" :"Douglas Crockford"
+
+    },
+    {
+        "q" : "5. What is correct MIME type for JSON ?",
+        "opt1" :" application/js",
+        "opt2" :" application/json",
+        "opt3" :" json/mime",
+        "answer" :"application/json"
+
+    },
+    {
+        "q" : "6. What extension in used to save a JSON file?",
+        "opt1" :".json",
+        "opt2" :".js",
+        "opt3" :".javaN",
+        "answer" :".json"
+
+    },
+    {
+        "q" : "7. Which of following statement is false about the space parameter in JSON.stringify() ?",
+        "opt1" :"It controls spacing in the resulting JSON string",
+        "opt2" :"It is an optional parameter",
+        "opt3" :" It removes whitespace",
+        "answer" :" It removes whitespace"
+
+    },
+    {
+        "q" : "8. Can you use a double quote inside a JSON string?",
+        "opt1" :"Yes, if you use the ascii code",
+        "opt2" :"Yes, if it is escaped like ( \" )",
+        "opt3" :" Yes, you can use it without any special treatment",
+        "answer" :"Yes, if it is escaped like ( \" )"
+
+    },
+    {
+        "q" : "9. What is the common usage of Json on modern websites?",
+        "opt1" :"To send and receive bits of data",
+        "opt2" :" To store information remotely",
+        "opt3" :"To store information locally",
+        "answer" :"To send and receive bits of data"
+
+    },
+    {
+        "q" : "10. What two main structures compose JSON?",
+        "opt1" :"Arrays and Objects",
+        "opt2" :" Key and value",
+        "opt3" :" Class and Object",
+        "answer" :"Arrays and Objects"
+
+    },
+];
